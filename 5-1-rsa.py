007b7000a142ea66a8bdb56c012eee7775925af2
Shivansh <ronozor1998@gmail.com>
2019-09-20 13:37:08 +0530

    .

--- 5-1-rsa.py
+++ 5-1-rsa.py
@@ -8,51 +8,44 @@ def gcd(a, h):
           return h 
         a = h 
         h = temp 
-  
-# Two random prime numbers 
+   
 p = 3 
 q = 7 
   
-# First part of public key: 
 n = p*q 
 
-# Finding other part of public key. 
-#  stands for encrypt 
-
+# Finding second part of publickey. 
 e = 2
-phi = (p-1)*(q-1)
+pq = (p-1)*(q-1)
 
-while (e < phi):
+while (e < pq):
     
-    # e must be co-prime to phi and 
-    # smaller than phi. 
+    # e must be co-prime to pq and smaller than pq. 
     
-    if (gcd(e, phi)==1):
+    if (gcd(e, pq)==1):
         break
     else:
         e=e+1
 
-# Private key (d stands for decrypt) 
-# choosing d such that it satisfies 
-# d*e = 1 + k * totient 
+# d*e = 1 + k * quotient 
 
-k = 2; # A constant value 
-d = (1 + (k*phi))/e 
+k = 2; #constant
 
-# Message to be encrypted 
-msg = 20 
+d = (1 + (k*pq))/e 
+ 
+msg = 10
 
-print("Message data =", msg) 
+print("Message =", msg) 
 
-# Encryption c = (msg ^ e) % n 
+#  c = (msg ^ e) % n 
 
 c = pow(msg, e)
 c = math.fmod(c, n) 
 print("\nEncrypted data =", c) 
 
-# Decryption m = (c ^ d) % n 
+#  m = (c ^ d) % n 
 
 m = pow(c, d) 
 m = math.fmod(m, n)
-print("\nOriginal Message Sent =", m)
+print("\nDecrypted Message =", m)
  
